2f62cd0064a61a4705a0251cbef0360ef36f26fc
William Njie <wsnjie@gmail.com>
2018-10-23 01:37:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,6 +13,6 @@ function logWhisper(string){
 function sayHiToGrandma(string){
 if (string.toLowerCase() === string){
   return "I can't hear you!"}
-  else if (string.toUpperCase === string){
+  else if (string.toUpperCase() === string){
     return "YES INDEED!"}
 }
