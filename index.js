e767571ad33eb9e29472fef8978d89a94d0e9d69
William Njie <wsnjie@gmail.com>
2018-10-23 01:42:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,4 +15,7 @@ if (string.toLowerCase() === string){
   return "I can't hear you!"}
   else if (string.toUpperCase() === string){
     return "YES INDEED!"}
+    else if (string === "I love you, Grandma."){
+      return "I love you, too."
+    }
 }
